817a09859cbf1da038721bcce799927e55abfa06
ananyae-24 <ananyae123@gmail.com>
2021-07-19 14:39:05 +0530

    problem4

--- server.js
+++ server.js
@@ -22,6 +22,10 @@ mongoose
 let app = express();
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
   // res.header("Access-Control-Allow-Headers", "X-Requested-With");
   // res.header("Access-Control-Allow-Headers", "Content-Type");
   // res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
